602374ef585c9c4da12d4c50c6e7edd4e05cade0
44yyp9 <ilovesoccer3939@gmail.com>
2024-07-26 17:29:34 +0900

    dev-editor-commit

--- Assets/test_scpript/Test_script.cs
+++ Assets/test_scpript/Test_script.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Test_script : MonoBehaviour
 {
+    float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        if (time < 1f) return;
+        Map_update_object.map_formupdate[1, 1] = 10;
+        Map_update_object.map_formupdate[15, 1] = 10;
+        Map_update_object.map_formupdate[3, 8] = 10;
+
     }
 }
