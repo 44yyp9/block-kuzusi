46663da01dab6cbb561ede65e45e0d8f02a4b431
44yyp9 <ilovesoccer3939@gmail.com>
2024-08-07 12:12:27 +0900

    Fixed_enter_stair_bug

--- Assets/game_stage/player/stair/Player_movig_stair.cs
+++ Assets/game_stage/player/stair/Player_movig_stair.cs
@@ -8,16 +8,19 @@ public class Player_movig_stair : MonoBehaviour
 {
     public void move_stair(int stair_num,int up_or_down)
     {
-        var map_form = Map_update_object.map_formupdate;
-        var _player_posi = gameObject.transform.position;
-        var _player_map_posi = Mapposition.isMapposition((int)_player_posi.x, (int)_player_posi.y);
-        if (map_form[_player_map_posi[0], _player_map_posi[1]] == stair_num)
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            if (up_or_down < -1 || up_or_down > 1 || up_or_down == 0) return; //ÉKÅ[Éhêﬂ
-            Map_update_object.hierarchy_num += up_or_down;
-            Game_end_loop.Initialise_map();
-            string current_scenename = SceneManager.GetActiveScene().name;
-            SceneManager.LoadScene(current_scenename);
+            var map_form = Map_update_object.map_formupdate;
+            var _player_posi = gameObject.transform.position;
+            var _player_map_posi = Mapposition.isMapposition((int)_player_posi.x, (int)_player_posi.y);
+            if (map_form[_player_map_posi[0], _player_map_posi[1]] == stair_num)
+            {
+                if (up_or_down < -1 || up_or_down > 1 || up_or_down == 0) return; //ÉKÅ[Éhêﬂ
+                Map_update_object.hierarchy_num += up_or_down;
+                Game_end_loop.Initialise_map();
+                string current_scenename = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(current_scenename);
+            }
         }
     }
 }
--- Assets/game_stage/player/stair/Player_up_stair.cs
+++ Assets/game_stage/player/stair/Player_up_stair.cs
@@ -11,17 +11,11 @@ public class Player_up_stair : MonoBehaviour
     void Start()
     {
         movig_stair = GetComponent<Player_movig_stair>();
-        _start_posi_x=(int)transform.position.x;
-        _start_posi_y=(int)transform.position.y;
-        boolean_start=false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var posi=transform.position;
-        if (_start_posi_x == (int)posi.x && _start_posi_y == (int)posi.y) { }else boolean_start=true;
-        if (!boolean_start) return; //ÉKÅ[Éhêﬂ
         movig_stair.move_stair(Map_update_object.inlet_stair_num, 1);
     }
 }
